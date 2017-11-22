f86b0030c7ac18bfc6174952da32d0cc513cb43e
asidun <asidun@gmail.com>
2017-11-22 10:02:24 +0200

    Return to List for store flowers

--- FlowerGarden/src/main/java/com/flowergarden/bouquet/MarriedBouquet.java
+++ FlowerGarden/src/main/java/com/flowergarden/bouquet/MarriedBouquet.java
@@ -1,23 +1,15 @@
 package com.flowergarden.bouquet;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import com.flowergarden.flowers.Chamomile;
 import com.flowergarden.flowers.GeneralFlower;
-import com.flowergarden.flowers.Rose;
 
 public class MarriedBouquet implements Bouquet<GeneralFlower> {
 
 	private float assemblePrice = 120;
-	private Map<String, Set<GeneralFlower>> flowerList = new HashMap<>();
+	private List<GeneralFlower> flowerList = new ArrayList<>();
 
 	@Override
 	public float getPrice() {
